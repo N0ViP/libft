a44a6fba153b8837f771330356c305d0f55b25ac
N0_01 <148581904+N0ViP@users.noreply.github.com>
2024-11-08 04:11:08 +0100

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stddef.h>
 
 int	ft_memcmp(const void *str1, const void *str2, size_t n)
 {
